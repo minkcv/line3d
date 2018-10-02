cc6a89ab3877d3b97f268b82efcf5fa55cee07e8
Will Smith <minkcv@gmail.com>
2018-10-02 13:19:23 -0600

    fix pan speed when zoomed out, start on picking points

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+// Disable right click context menu
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Handle keypresses
 var keysDown = [];
 var keys = { up: 38, down: 40, right: 39, left: 37, a: 65, s: 83, d: 68, w: 87, shift: 16, f: 70, space: 32, q: 81, z: 90, e: 69, r: 82}
 addEventListener("keydown", function(e) {
@@ -11,6 +14,7 @@ addEventListener("keyup", function(e) {
     delete keysDown[e.keyCode];
 }, false);
 
+// Set up 3D scene, camera, and renderer in THREE.js
 var threediv = document.getElementById('three');
 var width = threediv.clientWidth;
 var height = threediv.clientHeight;
@@ -19,12 +23,15 @@ var scene = new THREE.Scene();
 scene.background = new THREE.Color('#000000');
 
 var scale = 2;
-var camera = new THREE.OrthographicCamera(width / -scale, width / scale, height / scale, height / -scale, -4000, 4000);
+var realCamera = new THREE.OrthographicCamera(width / -scale, width / scale, height / scale, height / -scale, 0, 4000);
 
 var gridHelper = new THREE.GridHelper(1000, 10, 0x555555, 0x555555);
 scene.add(gridHelper);
 
 var cam = new THREE.Object3D(); // Parent for camera
+var camera = new THREE.Object3D(); // Parent for realCamera
+camera.add(realCamera);
+realCamera.translateZ(2000);
 cam.add(camera);
 scene.add(cam);
 var camAxes = new THREE.AxesHelper(50, 0.5);
@@ -32,26 +39,19 @@ cam.add(camAxes);
 var originAxes = new THREE.AxesHelper(50);
 scene.add(originAxes);
 
-var lineMat = new THREE.LineBasicMaterial({color: 0xffffff});
-var mat = new THREE.MeshBasicMaterial({color: 0x0000ff});
-function addLines() {
-    var pairs = getLines();
-    pairs.forEach((p) => {
-        var geom = new THREE.Geometry();
-        geom.vertices.push(new THREE.Vector3(p[0][0], p[0][1], p[0][2]));
-        geom.vertices.push(new THREE.Vector3(p[1][0], p[1][1], p[1][2]));
-        var line = new THREE.Line(geom, lineMat);
-        line.pointIds = [p[3], p[4]];
-        scene.add(line);
-    });
-}
-addLines();
+var whiteLineMat = new THREE.LineBasicMaterial({color: 0xffffff});
+var redLineMat = new THREE.LineBasicMaterial({color: 0xff0000});
+var greenLineMat = new THREE.LineBasicMaterial({color: 0x00ff00});
+var blueLineMat = new THREE.LineBasicMaterial({color: 0x0000ff});
 
 var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(width, height);
 threediv.appendChild(renderer.domElement);
 
-var moveSpeed = 1;
+var raycaster = new THREE.Raycaster();
+raycaster.linePrecision = 5;
+var mouseVec = new THREE.Vector2();
+
 var rotateFactor = 100;
 var sprintFactor = 1;
 var zoomSpeed = 0.2;
@@ -95,21 +95,22 @@ function update() {
         var viewRight = new THREE.Vector3();
         viewRight.crossVectors(dir, viewUp);
         var translate = new THREE.Vector3(
-            viewRight.x * mouseDX + viewUp.x * -mouseDY,
+            viewRight.x * -mouseDX + viewUp.x * -mouseDY,
             viewUp.y * -mouseDY,
-            viewUp.z * -mouseDY + viewRight.z * mouseDX);
+            viewUp.z * -mouseDY + viewRight.z * -mouseDX);
+        translate.multiplyScalar(1 / realCamera.zoom);
         cam.position.add(translate);
     }
     else if (mouseButton == 1) { // Middle click
         cam.translateY(-mouseDY);
     }
     if (mouseDZ < 0) { // Scroll wheel
-        camera.zoom *= 1 + zoomSpeed;
-        camera.updateProjectionMatrix();
+        realCamera.zoom *= 1 + zoomSpeed;
+        realCamera.updateProjectionMatrix();
     }
     else if (mouseDZ > 0) {
-        camera.zoom *= 1 - zoomSpeed;
-        camera.updateProjectionMatrix();
+        realCamera.zoom *= 1 - zoomSpeed;
+        realCamera.updateProjectionMatrix();
     }
     mouseDX = 0;
     mouseDY = 0;
@@ -167,10 +168,45 @@ function threeOut(event) {
     mouseDown = false;
 }
 
+function toggleGrid(checkbox) {
+    if (checkbox.checked) {
+        scene.add(gridHelper);
+    }
+    else {
+        scene.remove(gridHelper);
+    }
+}
+
+function toggleAxes(checkbox) {
+    if (checkbox.checked) {
+        scene.add(originAxes);
+    }
+    else {
+        scene.remove(originAxes);
+    }
+}
+
+function toggleCameraAxes(checkbox) {
+    if (checkbox.checked) {
+        cam.add(camAxes);
+    }
+    else {
+        cam.remove(camAxes);
+    }
+}
+
 function animate() {
     requestAnimationFrame( animate);
     update();
 
-    renderer.render(scene, camera);
+    mouseVec.x = ((mouseX - threediv.offsetLeft) / threediv.clientWidth) * 2 - 1;
+    mouseVec.y = -(mouseY / threediv.clientHeight) * 2 + 1;
+    raycaster.setFromCamera(mouseVec, realCamera);
+    var intersects = raycaster.intersectObjects(scene.children, true);
+    for (var i = 0; i < intersects.length; i++) {
+        console.log(intersects[i]);
+    }
+
+    renderer.render(scene, realCamera);
 }
 animate();
--- points.js
+++ points.js
@@ -20,6 +20,27 @@ var points = [
     }
 ];
 
+function addPoint() {
+    var pointContainer = new THREE.Object3D();
+    var xGripGeom = new THREE.Geometry();
+    xGripGeom.vertices.push(new THREE.Vector3(0, 0, 0));
+    xGripGeom.vertices.push(new THREE.Vector3(20, 0, 0));
+    var xGrip = new THREE.Line(xGripGeom, redLineMat);
+    var yGripGeom = new THREE.Geometry();
+    yGripGeom.vertices.push(new THREE.Vector3(0, 0, 0));
+    yGripGeom.vertices.push(new THREE.Vector3(0, 20, 0));
+    var yGrip = new THREE.Line(yGripGeom, greenLineMat);
+    var zGripGeom = new THREE.Geometry();
+    zGripGeom.vertices.push(new THREE.Vector3(0, 0, 0));
+    zGripGeom.vertices.push(new THREE.Vector3(0, 0, 20));
+    var zGrip = new THREE.Line(zGripGeom, blueLineMat);
+    pointContainer.add(xGrip);
+    pointContainer.add(yGrip);
+    pointContainer.add(zGrip);
+    pointContainer.position.copy(cam.position);
+    scene.add(pointContainer);
+}
+
 function getLines() {
     var pairs = [];
     points.forEach((p) => {
